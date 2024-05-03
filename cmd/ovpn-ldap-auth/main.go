3d91d650abc5a4cf0fcb5b220fefecd7f43cddea
Gerard Borst <gerard-borst@hotmail.com>
2024-05-03 22:26:13 +0200

    add license header

--- cmd/ovpn-ldap-auth/main.go
+++ cmd/ovpn-ldap-auth/main.go
@@ -1,6 +1,27 @@
+/*
+OpenVPN ldap auth - OpenVPN Ldap authentication
+
+Copyright (C) 2019 - 2021 Egbert Pot
+Copyright (C) 2021 - 2024 Gerard Borst
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
+	"io"
 	"log"
 	"log/slog"
 	"os"
@@ -33,6 +54,7 @@ func main() {
 	viper.SetConfigType("yaml")                  // REQUIRED if the config file does not have the extension in the name
 	viper.AddConfigPath(".")                     // optionally look for config in the working directory
 	viper.AddConfigPath("../../tests/openldap/") // optionally look in the tests directory
+	viper.AddConfigPath("./tests/openldap/")     // optionally look in the tests directory
 	viper.AddConfigPath("/etc/openvpn/auth/")    // path to look for the config file in
 
 	viper.SetDefault("ldapClient.UseSSL", true)
